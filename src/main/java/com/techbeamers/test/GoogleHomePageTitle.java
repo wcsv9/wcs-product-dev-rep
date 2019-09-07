ca70841629e31cc442faf2b4010e5827ebf58413
wcsv9 <34248478+wcsv9@users.noreply.github.com>
2019-09-08 00:24:02 +0500

    Update GoogleHomePageTitle.java

--- src/main/java/com/techbeamers/test/GoogleHomePageTitle.java
+++ src/main/java/com/techbeamers/test/GoogleHomePageTitle.java
@@ -24,7 +24,7 @@ public class GoogleHomePageTitle {
 	public void verifyGooglePageTittle() {
 		driver.navigate().to(URL);
 		String getTitle = driver.getTitle();
-		Assert.assertEquals(getTitle, "wWelcome to AuroraESite");
+		Assert.assertEquals(getTitle, "Gsss");
 	}
 	
 	@AfterClass
