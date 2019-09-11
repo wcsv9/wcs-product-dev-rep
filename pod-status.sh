21e740125cfe2677a5207d9ea2e666fd2c367eb3
wcsv9 <34248478+wcsv9@users.noreply.github.com>
2019-09-11 23:13:00 +0500

    Create pod-status.sh

---
+++ pod-status.sh
@@ -0,0 +1,30 @@
+#n=0
+export env=$1
+#status=`kubectl get deployment demoqaauthcrs-app |awk '{ print $5 }' | awk '{if (NR!=1) {print}}'`
+# until [ $n -ge 5 ]
+runtime="5 minute"
+endtime=$(date -ud "$runtime" +%s)
+
+while [[ $(date -u +%s) -le $endtime ]]
+   do
+   status=`kubectl get deployment  demoqaauth$1 |awk '{ print $5 }' | awk '{if (NR!=1) {print}}'`
+   if [ $status = 1 ]; then
+
+        echo "$status"
+        break;
+    else
+     echo "$status"
+
+     #n=$[$n+1]
+sleep 10
+fi
+done
+
+if [ $status = 1 ]; then
+
+echo "POD is UP"
+
+else
+echo "POD is not coming up"
+exit 3;
+fi
