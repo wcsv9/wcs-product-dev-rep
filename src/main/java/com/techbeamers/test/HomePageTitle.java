23af1869c9d1aaff39783019c3b1b6ddbe55efc3
wcsv9 <34248478+wcsv9@users.noreply.github.com>
2019-09-16 17:56:48 +0500

    Update HomePageTitle.java

--- src/main/java/com/techbeamers/test/HomePageTitle.java
+++ src/main/java/com/techbeamers/test/HomePageTitle.java
@@ -1,5 +1,6 @@
 package com.techbeamers.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -8,32 +9,76 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
+
 public class HomePageTitle {
 	
-	private WebDriver driver; 
+	private static WebDriver driver; 
 	String URL = "https://wcs.demoqaauth.ibm.com/wcs/shop/en/auroraesite";
 
 	@BeforeClass
 	public void testSetUp() {
-		System.setProperty("webdriver.firefox.driver","/data/wcscode/geckodriver"); // <– Change this path
-		
+		try{
+		System.setProperty("webdriver.firefox.driver","/data/wcscode/geckodriver"); // <â€“ Change this path
 		driver = new FirefoxDriver();
+			   }
+		catch(Exception e)
+		{
+			e.printStackTrace();  
+		}
 	}
 	
-	@Test
-	public void verifyPageTittle() {
+	@Test(priority = 0)
+	public void verifyPageTittle() throws Exception  {
+		try{
 		driver.navigate().to(URL);
 		//String getTitle = driver.getTitle();
 		//Assert.assertEquals(getTitle, "Welcome to AuroraESite");
 		//Assert.assertEquals(getTitle, "GGG");
 		String Expectedtitle = "Welcome to AuroraESite";
-//it will fetch the actual title 
-String Actualtitle = driver.getTitle();
-System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
-//it will compare actual title and expected title
-Assert.assertEquals(Actualtitle, Expectedtitle);
-//print out the result
-System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+        //it will fetch the actual title 
+        String Actualtitle = driver.getTitle();
+        System.out.println("\nBefore Assetion= " + Expectedtitle + Actualtitle);
+        //it will compare actual title and expected title
+        Assert.assertEquals(Actualtitle, Expectedtitle);
+        //print out the result
+        System.out.println("After Assertion= " + Expectedtitle + Actualtitle + "\nTitle matched ");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();  
+		}
+	}
+	
+	@Test(priority = 1)
+	public static void Search() throws Exception  {
+		try{
+			Thread.sleep(5000);
+			driver.findElement(By.id("SimpleSearchForm_SearchTerm")).click();
+//			driver.findElement(By.id("SimpleSearchForm_SearchTerm")).clear();
+			driver.findElement(By.id("SimpleSearchForm_SearchTerm")).sendKeys("Shoes");
+			driver.findElement(By.xpath("//*[@id=\"searchBox\"]/a[2]")).click();
+			Thread.sleep(5000);
+			String ActualText = driver.findElement(By.xpath("//*[@id='widget_breadcrumb']/ul/li[2]")).getText();
+			Assert.assertEquals(ActualText, "Search: Shoes","Expected text not found on page!");
+			System.out.print("\nSearch Executed");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();  
+		}
+	}
+	
+	@Test(priority = 2)
+	public static void PDP() throws Exception  {  
+		try{
+			driver.findElement(By.xpath("(//a[@id='WC_CatalogEntryDBThumbnailDisplayJSPF_11948_link_9b']")).click();
+			System.out.print("\nPDP Executed");
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();  
+		}
 	}
 	
 	//@AfterClass
@@ -41,4 +86,3 @@ System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title ma
 	//	driver.quit();
 	//}
 }
-
